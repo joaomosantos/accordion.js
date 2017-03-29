fb468a42d7b3dfcf757fb5a04d80056810383535
joaomosantos <joaomosantos@outlook.com>
2017-03-28 21:53:06 -0300

    Otimizando main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function() {
 	$('.accordion').accordion();
-	$('pre').mCustomScrollbar({
-		axis:"x",
-		theme:"light",
-		scrollbarPosition: "outside"
-	});
 
-	$('.accordion-control').click(function(event) {
-		event.preventDefault();
-		$(this).toggleClass('opened');
-	});
+  $('.accordion-control').click(function() {
+    $(this).toggleClass('opened');
+  });
+
+  $('pre').mCustomScrollbar({
+    axis:"x",
+    theme:"light",
+    scrollbarPosition: "outside"
+  });
 });
